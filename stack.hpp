0406c09b314d2846cccdbb1b6e6202e8cf5e1a71
ImKool-Gitch <71771827+ImKool-Gitch@users.noreply.github.com>
2023-04-21 12:26:05 +0200

    Update stack.hpp

--- stack.hpp
+++ stack.hpp
@@ -1,18 +1,21 @@
+#ifndef STACKHPP
+
+#define STACKHPP
+
+template <typename T>
 class Stack {
 private:
 	T* stack;
-	// i is a counter for how many "stack frames" are in the stack
+	// i is a counter for how many elements are in the stack
 	unsigned i;
 public:
 	Stack() {
 		stack = nullptr;
 		i = 0;
 	}
-
 	~Stack() {
 		delete[] stack;
 	}
-
 	void push(T data) {
 		//If there are no elements, the stack is allocated for one element and the new element is copied the first element. In the end i is incremented
 		if (i == 0) {
@@ -29,15 +32,23 @@ public:
 			T* temp_stack = new T[i];
 			for (unsigned j = 0; j < i; ++j)
 				temp_stack[j] = stack[j];
+			delete[] stack;
+			stack = new T[i + 1];
+			stack[0] = data;
+			for (unsigned j = 1; j < i + 1; ++j) {
+				stack[j] = temp_stack[j - 1];
+			}
+			++i;
 			delete[] temp_stack;
 		}
 	}
-
 	void pop() {
 		//If there is only one element, the stack is deallocated then nullptr is assigned to it, to indicate that its empty, then i is decremented.
 		if (i == 1) {
 			delete[] stack;
 			stack = nullptr;
+			--i;
+		}
 		else if (i == 0)
 			return;
 		else {
@@ -50,24 +61,28 @@ public:
 			T* temp_stack = new T[i - 1];
 			for (unsigned j = 0; j < i - 1; ++j)
 				temp_stack[j] = stack[j + 1];
+			delete[] stack;
+			stack = new T[i - 1];
+			for (unsigned j = 0; j < i - 1; ++j)
+				stack[j] = temp_stack[j];
+			delete[] temp_stack;
 			--i;
 		}
 	}
-
 	void print_stack() {
 		for (unsigned j = 0; j < i; ++j)
 			std::cout << stack[j] << std::endl;
 	}
-
 	void peek() {
 		if (is_empty())
 			return;
 		std::cout << stack[0] << std::endl;
 	}
-
 	bool is_empty() {
 		if (i == 0)
 			return true;
 		return false;
 	}
 };
+
+#endif
